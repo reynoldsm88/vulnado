9b5ab63213c95ff942a6705a7948aa8f3d4cf403
Michael Reynolds <reynoldsm88@gmail.com>
2023-03-10 12:31:49 -0500

    Initialize pixeeworks-drip-2023-03-07-pixee-java/limit-readline

---
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -0,0 +1,29 @@
+package com.scalesec.vulnado;
+
+import io.openpixee.security.BoundedLineReader;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Cowsay {
+  public static String run(String input) {
+    ProcessBuilder processBuilder = new ProcessBuilder();
+    String cmd = "/usr/games/cowsay '" + input + "'";
+    System.out.println(cmd);
+    processBuilder.command("bash", "-c", cmd);
+
+    StringBuilder output = new StringBuilder();
+
+    try {
+      Process process = processBuilder.start();
+      BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+      String line;
+      while ((line = BoundedLineReader.readLine(reader, 1000000)) != null) {
+        output.append(line + "\n");
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return output.toString();
+  }
+}
