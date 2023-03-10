9a7ca6ee73ae53e227ff87e85072a6d03da5dfeb
Michael Reynolds <reynoldsm88@gmail.com>
2023-03-10 12:31:47 -0500

    Initialize xss

---
+++ src/main/java/com/scalesec/vulnado/LinksController.java
@@ -0,0 +1,18 @@
+package com.scalesec.vulnado;
+
+import org.springframework.boot.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.autoconfigure.*;
+import java.util.List;
+import java.io.Serializable;
+
+@RestController
+@EnableAutoConfiguration
+public class LinksController {
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "/links", produces = "application/json")
+    List<String> links(@RequestParam String url) {
+        return LinkLister.getLinks(url);
+    }
+}
